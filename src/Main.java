7d714393d834cccc0034578d7ba393fdcf62224d
Kenneth.Ajah <kenneth.ajah@stanbicibtc.com>
2024-01-12 00:06:55 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Opt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+//        ArrayList<int> al = ArrayList<int>(); // does not work
+        ArrayList<Integer> al = new ArrayList<Integer>(); // works fine
+    }
+}
---
+++ src/listInterface/arraylist/ArrayListExample1.java
@@ -0,0 +1,14 @@
+package listInterface.arraylist;
+
+import java.util.*;
+ public class ArrayListExample1{  
+ public static void main(String args[]){  
+  ArrayList<String> list=new ArrayList<String>();//Creating listInterface.arraylist
+      list.add("Mango");//Adding object in listInterface.arraylist
+      list.add("Apple");    
+      list.add("Banana");    
+      list.add("Grapes");    
+      //Printing the listInterface.arraylist object
+      System.out.println(list);  
+ }  
+}  
